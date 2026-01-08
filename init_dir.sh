1347f42b5d46b3889afcd4b6c69f8cf20064ce0e
etluser <etluser@node1.littletask.com>
2026-01-08 11:25:27 +0900

    커밋 메시지

---
+++ init_dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p /app/poc/mcps/
+mkdir -p /app/poc/mcps/shared/
+mkdir -p /app/poc/mcps/config/
+mkdir -p /app/poc/mcps/data/
+mkdir -p /app/poc/mcps/mcp-tools/
+mkdir -p /app/poc/mcps/mcp-servers/core/
+mkdir -p /app/poc/mcps/mcp-host/
+mkdir -p /app/poc/mcps/api-gateway/
+mkdir -p /app/poc/mcps/frontend/
+mkdir -p /app/poc/mcps/scripts/
+mkdir -p /app/poc/mcps/tests/
